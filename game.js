ee37a94750ae927f6ca5e0900f263e1dde11c0c1
victor.teixido <victor.teixido@est.fib.upc.edu>
2022-05-03 22:14:40 +0200

    some updates

--- game.js
+++ game.js
@@ -1,74 +1,64 @@
-let color_final;
-let col_display;
-let display_mix;
+let color_final;    // color shown as objective
+let col_display;    // colors used to create player's color
+let display_mix;    // color created by the player
+
 let time;
-let intervalid;
+let intervalId;
+countdownEl = document.getElementById('countdown');
 
+function updateCountdown() {
+    time--;
+    if (time > 0) {
+        time = time < 10 ? '0' + time : time;
+        countdownEl.innerHTML = `00:${time}`;
+        time = time < 0 ? 0 : time;
+    }
+    else {
+        clearInterval(intervalId);
+        let accuracy = checkAccuracy();
+        var answer = window.confirm("You lose :( \nYou had an accuracy of " + parseFloat(accuracy).toFixed(2) + "% \n Want to play again?");
+        if (answer) runGame();
+        else showMenu();
+    }
+}
 
+function MyConfirm() {
+    if (intervalId) clearInterval(intervalId);
+    if (window.confirm("You WIN!!! \n Want to play again?")) {
+        runGame();
+    }
+    else showMenu();   
+}
 
 function runGame() {
     document.getElementById('game').style.display = "";
 
-    clean();
     time = 30;
+    countdownEl.innerHTML = `00:${time}`;
+    intervalId = setInterval(updateCountdown, 1000);
+
+    clean();
     generarColorAleatorio();
 
-    const canvas = document.getElementById("canvas")
-    canvas.height = window.innerHeight
-    canvas.width = window.innerWidth
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
 
-    const ctx = canvas.getContext("2d")
-
-
-    const countdownEl = document.getElementById('countdown');
-    intervalid = setInterval(updateCountdown, 1000);
-
-    function updateCountdown() {
-        if (time > 0) {
-            time = time < 10 ? '0' + time : time;
-
-            countdownEl.innerHTML = `00:${time}`;
-            time--;
-            time = time < 0 ? 0 : time;
-        }
-        else {
-            let accu = checkAccuracy();
-            var answer = window.confirm("You lose :( \nYou had an accuracy of " + parseFloat(accu).toFixed(2) + "% \n Want to play again?");
-            if (intervalid) clearInterval(intervalid);
-            if (answer) runGame();
-
-            else
-             showMenu();
-        }
-    }
-    function MyConfirm() {
-        if (intervalid) clearInterval(intervalid);
-        if (window.confirm("You WIN!!! \n Want to play again?")) {
-            //clean();
-            runGame();
-        }
-        else {
-            showMenu();
-            
-        }
-
-    }
-
-    let clrs = document.querySelectorAll(".clr")
-    clrs.forEach(clr => {
+    let colors = document.querySelectorAll(".clr");
+    colors.forEach(clr => {
         clr.addEventListener("click", () => {
-            ctx.strokeStyle = clr.dataset.clr
-            console.log(ctx.strokeStyle)
+            ctx.strokeStyle = clr.dataset.clr;
+            console.log(ctx.strokeStyle);
             col_display.push(clr.dataset.clr);
-            if (col_display.length != 1) {
+            
+            //if (col_display.length != 1) {
                 display_mix = averageHex(col_display);
                 document.querySelector(".display").style.backgroundColor = display_mix;
-            }
+            /*}
             else {
                 document.querySelector(".display").style.backgroundColor = clr.dataset.clr;
             }
+            */
             if (checkAccuracy() >= 96) {
-                //clean();
                 window.setTimeout(MyConfirm, 300);
             }
         })
